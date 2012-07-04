34afff96be804fd35ac07f450ed33d8c1be7c5a3
Rment <rediatpm@gmail.com>
2012-07-04 11:46:41 -0700

    critical whitespace bug fixed

--- githtml.js
+++ githtml.js
@@ -38,11 +38,10 @@
       var pr = hp.replace(/<div class=[\'|\"]line/g,'\n<div class="line').replace(/(<([^>]+)>)/ig,'');
           pr = pr.replace(/[a-zA-Z0-9-_\.\/\:]+bootstrap[a-zA-Z0-9-_\.\/\:]+/g,link.bootstrap);
           pr = escape(gitHtml.decode(el,pr));
-          pr = pr.replace(/http/g,'https');
+          pr = pr.replace(/http/g,'https'); 
+  	  pr = unescape(pr).replace(/\n/g,'--newline--').replace(/\s/g,' ').replace(/--newline--/g,'\n');
       gitHtml.open(1000,700,
-        '<scr'+'ipt>'+
-          'document.write(unescape("%3Cbase%20href%3D%22'+escape(lo)+'%22%3E'+pr+'"));'+
-        '</scr'+'ipt>'
+        unescape("%3Cbase%20href%3D%22"+escape(lo)+"%22%3E")+pr
       );
     })
   }
