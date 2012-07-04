3795933004d5c809796a775b421f17421c9d05f2
Rment <rediatpm@gmail.com>
2012-07-04 14:13:51 -0700

    fixed navigation issues

--- githtml.js
+++ githtml.js
@@ -16,7 +16,6 @@
       return str;
     }),
     open: (function(c,d) {
-      if(!d.getElementById('iframe')){
         var ifr                = d.createElement('iframe');
             ifr.src            = 'about:blank';
             ifr.style.width    = '100%';
@@ -25,10 +24,23 @@
             ifr.style.top      = '0px';
             ifr.style.zIndex   = '9999';
             ifr.id             = 'iframe';
+       var  close                = d.createElement('a');
+            close.innerHTML      = 'x';
+            close.style.position = 'absolute';
+            close.style.top      = '5px';
+            close.style.left     = '5px';
+            close.style.zIndex   = '99999';
+            close.className      = 'minibutton';
+            close.href           = 'javascript:;';
+            close.onclick        = (function(){
+              d.getElementById('iframe').style.display = 'none';
+              d.removeChild(d.getElementById('iframe'));
+              return false;
+            });
+        d.body.appendChild(close);
         d.body.appendChild(ifr);
         d.body.style.padding   = '0px';
         d.body.style.margin    = '0px';
-      }
       var ifrm = d.getElementById('iframe');
           ifrm = (ifrm.contentWindow) 
                ? ifrm.contentWindow 
@@ -62,7 +74,4 @@
     })
   }
   gitHtml.start(d);
-  document.onbeforeunload = function(){
-    document.getElementById('iframe').style.display = 'none';
-  }  
 })(document);
